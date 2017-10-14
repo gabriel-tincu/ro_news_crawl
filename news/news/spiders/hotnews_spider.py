efe463cb79259b75479fc051de67fef905203451
Tincu Gabriel <tincu@eloquentix.com>
2017-10-14 16:47:28 +0300

    fix error cause in hotnews

--- news/news/spiders/hotnews_spider.py
+++ news/news/spiders/hotnews_spider.py
@@ -12,13 +12,14 @@ class HotnewsSpider(Spider):
         extractor = LinkExtractor(allow_domains=['www.hotnews.ro', 'hotnews.ro'])
         title = response.css('h1::text').extract_first()
         author = response.css('.autor a::text').extract_first()
-        tags = [x.lower().strip() for x in response.css('.atual::text').extract_first().split('|')]
+        tags = response.css('.atual::text').extract_first()
         date_ro = response.css('.data::text').extract_first()
         date_en = response.xpath('//meta[@name="DC.date.issued"]/@content').extract_first()
         intro = response.css('#articleContent strong::text').extract_first()
         body = response.css('#articleContent div+ div::text').extract()
         body = '\n'.join(x.strip() for x in body)
         if title and author and tags and intro and body:
+            tags = [x.lower().strip() for x in tags.split('|')]
             data = {
                 'url': response.url,
                 'author': author.strip(),
