20e1654c36c3a37d83d7a3b0c03ee426fa024ab7
Tincu Gabriel <tincu@eloquentix.com>
2017-10-16 19:57:06 +0300

    use default settings

--- news/main.py
+++ news/main.py
@@ -6,25 +6,11 @@ from news.spiders.digi_spider import DigiSpider
 from news.spiders.tvr_spider import TVRSpider
 from news.spiders.protv_spider import ProTVSpider
 from news.spiders.realitatea_spider import RealitateaSpider
-
-
-import news.settings as default_settings
-
 from scrapy.utils.log import configure_logging
 from scrapy.settings import Settings
 
-s = Settings({
-    'MONGO_URL': 'mongo',
-    'MONGO_COLLECTION': default_settings.MONGO_COLLECTION,
-    'ITEM_PIPELINES': default_settings.ITEM_PIPELINES,
-    'ES_URL': 'elasticsearch',
-    'DOWNLOAD_DELAY': default_settings.DOWNLOAD_DELAY,
-    'CONCURRENT_REQUESTS_PER_DOMAIN': default_settings.CONCURRENT_REQUESTS_PER_DOMAIN,
-    'BOT_NAME': default_settings.BOT_NAME,
-    'SPIDER_MODULES': default_settings.SPIDER_MODULES,
-    'NEWSPIDER_MODULE': default_settings.NEWSPIDER_MODULE,
-    'LOG_LEVEL': 'INFO'
-})
+s = Settings()
+s.setmodule('news.settings')
 
 configure_logging(s)
 
