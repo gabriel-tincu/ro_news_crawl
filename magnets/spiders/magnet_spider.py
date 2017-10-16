a310a5bb8cbc8412ab92912325fee7a9875ec6be
Tincu Gabriel <tincu@eloquentix.com>
2017-10-17 01:48:38 +0300

    add magnet crawler, since i'm too lazy to make a new repo for that

---
+++ magnets/spiders/magnet_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+from urllib.parse import (
+    parse_qs,
+    urlparse
+)
+
+class MagnetSpider(scrapy.Spider):
+    start_urls = [
+        ''
+    ]
+    allowed_domains = [
+        ''
+    ]
+
+    def parse(self, response):
+        links = [urlparse(x) for x in response.xpath('//a/@href').extract()]
+        magnets = [parse_qs(x.query) for x in links if x.scheme == 'magnet']
+        follows = [parse_qs(x.query) for x in links if x.scheme != 'magnet']
+        yield magnets
+        for x in follows:
+            yield scrapy.Request(x, callback=self.parse)
